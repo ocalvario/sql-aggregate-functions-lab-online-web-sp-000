477596f197bcd59b44d2d7925dec378891289775
Oswald Calvario <oswald.calvario@gmail.com>
2020-04-26 01:23:39 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,12 +9,13 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  "SELECT AVG (gpa) from STUDENTS"
+  "SELECT AVG (gpa) from students"
 end
 
 def total_tardies_for_all_students
-  
+  "SELECT SUM (tardies) from students"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT average (gpa) from students WHERE grade = 9"
 end
